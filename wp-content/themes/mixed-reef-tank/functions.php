196af88df2e5261e31fba3abce49643f05cc4d32
Cody Sparks <cosparks@live.com>
2024-06-28 17:11:28 -0600

    Header layout

--- wp-content/themes/mixed-reef-tank/functions.php
+++ wp-content/themes/mixed-reef-tank/functions.php
@@ -159,11 +159,6 @@ function mixed_reef_tank_scripts() {
 add_action( 'wp_enqueue_scripts', 'mixed_reef_tank_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
--- wp-content/themes/mixed-reef-tank/header.php
+++ wp-content/themes/mixed-reef-tank/header.php
@@ -24,26 +24,12 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$mixed_reef_tank_description = get_bloginfo( 'description', 'display' );
-			if ( $mixed_reef_tank_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $mixed_reef_tank_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			<div class="site-logo">
+				<?php the_custom_logo(); ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-name" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</div>
 
-		<nav id="site-navigation" class="main-navigation">
+			<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mixed-reef-tank' ); ?></button>
 			<?php
 			wp_nav_menu(
