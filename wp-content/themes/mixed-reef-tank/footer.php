e8fcc17ecae12ea32800ae6c08b5a46987a7cb08
Cody Sparks <cosparks@live.com>
2024-08-09 14:47:21 -0600

    Google Tag Manager condition

--- wp-content/themes/mixed-reef-tank/footer.php
+++ wp-content/themes/mixed-reef-tank/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,62 +12,75 @@
 
 ?>
 
-	<div id="before_footer"><img src="/wp-content/themes/mixed-reef-tank/img/wave.svg" alt="Footer Design" /></div>
-	
-	<footer id="colophon" class="site-footer">
-		<div class="content-wrapper">
-			<div class="columns">
-				<div class="column">
-					<p><strong>Mixed Reef Tank</strong></p>
+<div id="before_footer"><img src="/wp-content/themes/mixed-reef-tank/img/wave.svg" alt="Footer Design" /></div>
 
-					<p>With over a decade of experience in saltwater aquariums, find expert articles in fish care and coral health.</p>
-				</div>
+<footer id="colophon" class="site-footer">
+	<div class="content-wrapper">
+		<div class="columns">
+			<div class="column">
+				<p><strong>Mixed Reef Tank</strong></p>
 
-				<div class="column">
-					<p><strong>Learn</strong></p>
-					
-				<?php
-					wp_nav_menu(array(
-						'theme_location' => 'menu-2',
-						'menu_id'        => 'menu-2',
-						'menu_class'     => 'footer-menu',
-					));
-				?>
-				</div>
-
-				<div class="column">
-					<p><strong>Resources</strong></p>
-
-					<?php
-				
-					wp_nav_menu(array(
-						'theme_location' => 'menu-3',
-						'menu_id'        => 'menu-3',
-						'menu_class'     => 'footer-menu',
-					));
-				?>
-				</div>
+				<p>With over a decade of experience in saltwater aquariums, find expert articles in fish care and coral health.</p>
 			</div>
 
-			<hr>
-			<p class="copyright"><em>Copyright <?= date('Y'); ?> · All rights reserved</em></p>
+			<div class="column">
+				<p><strong>Learn</strong></p>
+
+				<?php
+				wp_nav_menu(array(
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'menu-2',
+					'menu_class'     => 'footer-menu',
+				));
+				?>
+			</div>
+
+			<div class="column">
+				<p><strong>Resources</strong></p>
+
+				<?php
+
+				wp_nav_menu(array(
+					'theme_location' => 'menu-3',
+					'menu_id'        => 'menu-3',
+					'menu_class'     => 'footer-menu',
+				));
+				?>
+			</div>
 		</div>
-	</footer><!-- #colophon -->
+
+		<hr>
+		<p class="copyright"><em>Copyright <?= date('Y'); ?> · All rights reserved</em></p>
+	</div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5FJHM7M9');</script>
-<!-- End Google Tag Manager -->
- <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FJHM7M9"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
- 
+<?php if ($_SERVER['SERVER_NAME'] == 'mixedreeftank.com') : ?>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-5FJHM7M9');
+	</script>
+	<!-- End Google Tag Manager -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FJHM7M9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php endif; ?>
+
 </body>
+
 </html>
