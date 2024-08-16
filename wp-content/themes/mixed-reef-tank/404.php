693c06fee5ccd0cdb83e11e970152474c4e1aef2
Cody Sparks <cosparks@live.com>
2024-08-16 16:43:15 -0600

    Update 404 error page image

--- wp-content/themes/mixed-reef-tank/404.php
+++ wp-content/themes/mixed-reef-tank/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,24 +11,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<div class="content-wrapper">
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mixed-reef-tank' ); ?></h1>
-				</header><!-- .page-header -->
+<main id="primary" class="site-main">
+	<div class="content-wrapper">
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'mixed-reef-tank'); ?></h1>
+			</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'The page you are looking for might be renamed, removed or might never existed in this tank.', 'mixed-reef-tank' ); ?></p>
+			<div class="page-content">
+				<p><?php esc_html_e('The page you are looking for might be renamed, removed or might never existed in this tank.', 'mixed-reef-tank'); ?></p>
 
-					<?php get_search_form(); ?>
+				<?php get_search_form(); ?>
 
-					<p><img src="/wp-content/uploads/2024/07/clownfish-sleeping.png" alt="Clownfish Sleeping" /></p>
+				<p><img src="/wp-content/uploads/2024/08/clownfish-sleeping.webp" alt="Clownfish Sleeping" /></p>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</div>
-	</main><!-- #main -->
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
+	</div>
+</main><!-- #main -->
 
 <?php
 get_footer();
